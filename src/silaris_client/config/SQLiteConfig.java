f38f8948b82e8b298b63136e08c0dfb74ef89eca
sindelarastechnology <sindelarstechnology@gmail.com>
2026-02-24 00:15:56 +0700

    Ganti nama Database

--- src/silaris_client/config/SQLiteConfig.java
+++ src/silaris_client/config/SQLiteConfig.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class SQLiteConfig {
     public static Connection connect() {
         try {
-            String url = "jdbc:sqlite:data/silaris.db";
+            String url = "jdbc:sqlite:data/silaris_client.db";
             return DriverManager.getConnection(url);
         } catch (Exception e) {
             System.out.println("Koneksi gagal: " + e.getMessage());

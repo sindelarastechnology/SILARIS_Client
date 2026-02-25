d6591e9d98548b09d789c4c486f9b9b9b88891a4
sindelarastechnology <sindelarstechnology@gmail.com>
2026-02-25 11:57:58 +0700

    Backup Pertama

--- src/silaris_client/service/PengirimanService.java
+++ src/silaris_client/service/PengirimanService.java
@@ -4,6 +4,9 @@ import okhttp3.*;
 import org.json.JSONArray;
 import silaris_client.config.configSupabase;
 import silaris_client.session.SessionSQLite;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class PengirimanService {
 
@@ -20,7 +23,7 @@ public class PengirimanService {
             "&rs_id=eq."+idRs+
             "&kode_rs=eq."+kodeRs+
             "&status=eq."+status+
-            "&select=pengiriman_id,rs_id,kode_rs,linen_dikirim(*)";
+            "&select=pengiriman_id,rs_id,kode_rs,total_linen,linen_dikirim(*)";
 
             Request request = new Request.Builder()
                     .url(url)
@@ -39,4 +42,56 @@ public class PengirimanService {
         }
         return null;
     }
+    
+    public static boolean updateStatusPengiriman(String pengirimanId){
+
+        try{
+
+            OkHttpClient client = configSupabase.client;
+
+            String url = configSupabase.SUPABASE_URL +
+                    "pengiriman?pengiriman_id=eq." + pengirimanId;
+            
+            ZoneId zone = ZoneId.of("Asia/Jakarta");
+
+            ZonedDateTime now = ZonedDateTime.now(zone);
+
+            String tanggal = now.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+
+            String json = "{"
+                    + "\"status\":\"DITERIMA\","
+                    + "\"tanggal_diterima\":\"" + tanggal + "\""
+                    + "}";
+
+            MediaType JSON = MediaType.parse("application/json");
+
+            RequestBody body = RequestBody.create(JSON, json);
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .patch(body)
+                    .addHeader("apikey", configSupabase.SUPABASE_API_KEY)
+                    .addHeader("Authorization",
+                            "Bearer " + configSupabase.SUPABASE_API_KEY)
+                    .addHeader("Prefer", "return=minimal")
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            if(response.isSuccessful()){
+                return true;
+            }else{
+                System.out.println("Update gagal: "
+                        + response.code()
+                        + " - "
+                        + response.body().string());
+            }
+
+        }catch(Exception e){
+            System.out.println("Error update status: "+e.getMessage());
+        }
+
+        return false;
+    }
+    
 }
